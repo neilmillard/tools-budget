0dd77510705d65bce497442489fa7b79c3230949
neil millard <3050649+neilmillard@users.noreply.github.com>
2025-03-11 14:58:37 +0000

    enable static website export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    /* After running next build, Next.js will create an out folder with the HTML/CSS/JS assets for your application.*/
+    output: 'export',
 };
 
 export default nextConfig;
