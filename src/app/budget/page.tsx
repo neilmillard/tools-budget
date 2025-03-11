5b4493c42b9bed4f82572037b244af6e4a6229d5
neil millard <3050649+neilmillard@users.noreply.github.com>
2025-03-11 15:06:37 +0000

    move budget to subfolder and add nav button to front page

---
+++ src/app/budget/page.tsx
@@ -0,0 +1,7 @@
+import BudgetPlanner from "@/app/components/BudgetPlanner";
+
+export default function Budget() {
+    return (
+        <BudgetPlanner />
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import BudgetPlanner from "@/app/components/BudgetPlanner";
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <BudgetPlanner />
+        <div className="w-[83%] mx-auto p-6 bg-white rounded-2xl shadow-md mt-10">
+            <h2 className="text-xl font-bold mb-4">Money Tools</h2>
+            <button className={"p-2 rounded bg-blue-500 text-white"} >
+                <Link href={`budget`}>Budget Planner</Link>
+            </button>
+        </div>
     )
 }
