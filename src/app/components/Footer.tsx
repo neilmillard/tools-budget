71ca2ec9b7b5bb3bd1123a0c6aacf6046442d285
Neil Millard <neil.millard@gmail.com>
2025-03-18 22:02:00 +0000

    update footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,6 +2,6 @@ import Link from "next/link";
 
 export function Footer() {
     return <div className="pt-4 flex h-full flex-row justify-center">
-        Made with Love by <Link href="https://neilmillard.com" className="pl-1">Neil Millard</Link>
+        Assembled by <Link href="https://neilmillard.com" className="pl-1">Neil Millard</Link>
     </div>
 }
